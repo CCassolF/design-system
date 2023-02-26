473b8683fe8fe111a4107aed95e42d77bde0bbd5
Carlos Cassol <carloscassolfilho@gmail.com>
2023-02-26 03:23:56 -0300

    Componente: Text

--- packages/docs/src/stories/Box.stories.tsx
+++ packages/docs/src/stories/Box.stories.tsx
@@ -1,15 +1,11 @@
 import type { Meta, StoryObj } from '@storybook/react'
-import { Box, BoxProps } from '@ignite-ui/react'
+import { Box, BoxProps, Text } from '@ignite-ui/react'
 
 export default {
   title: 'Surfaces/Box',
   component: Box,
   args: {
-    children: (
-      <>
-        <span>Testando o elemento</span>
-      </>
-    ),
+    children: <Text>Testando o elemento</Text>,
   },
 } as Meta<BoxProps>
 
---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum molestias dolorum, sequi perferendis voluptates ullam est dicta iure obcaecati nemo tempore dolorem ad id illum aliquid amet excepturi earum quo.',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}

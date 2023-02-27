7b93ff4e806fe7f57333fb142773b4b73073a017
Carlos Cassol <carloscassolfilho@gmail.com>
2023-02-27 01:59:52 -0300

    Controles nos componentes

--- packages/docs/src/stories/Text.stories.tsx
+++ packages/docs/src/stories/Text.stories.tsx
@@ -5,8 +5,31 @@ export default {
   title: 'Typography/Text',
   component: Text,
   args: {
+    size: 'md',
     children:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum molestias dolorum, sequi perferendis voluptates ullam est dicta iure obcaecati nemo tempore dolorem ad id illum aliquid amet excepturi earum quo.',
+  },
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+      control: {
+        type: 'inline-radio',
+      },
+    },
   },
 } as Meta<TextProps>
 
